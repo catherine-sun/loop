39caab0e27cf08eef26808f80130f9a95778e619
catherine-sun <59343226+catherine-sun@users.noreply.github.com>
2025-08-01 21:05:03 -0400

    feat: draggable rope (glitchy) (#6)
    * feat: draggable end
    
    * feat: glitchy rope

---
+++ scripts/rope/rope_end.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+var dragging = false
+var of = Vector2.ZERO
+var viewport_size
+var size
+
+func _ready() -> void:
+	size = $CollisionShape2D.shape.size
+	viewport_size = get_viewport_rect().size
+
+func _process(_delta: float) -> void:
+	if dragging:
+		var pos = get_global_mouse_position() - of
+		position = Vector2(
+			clamp(pos.x, size.x/2, viewport_size.x - size.x/2),
+			clamp(pos.y, size.y/2, viewport_size.y - size.y/2)
+		)
+
+func _on_button_button_down() -> void:
+	dragging = true
+	of = get_global_mouse_position() - global_position
+
+func _on_button_button_up() -> void:
+	dragging = false
