bb4dd1cf677e51084ee328f0d3209025349229d1
catherine-sun <59343226+catherine-sun@users.noreply.github.com>
2025-08-01 22:59:04 -0400

    feat: stiff joints near holder, increase drag (#9)

--- scripts/rope/rope_generator.gd
+++ scripts/rope/rope_generator.gd
@@ -2,47 +2,54 @@ extends Node2D
 
 @export var ropeLength: int
 @export var ropeSegmentWidth: int
+@export var jointStiffness: float
 
 const ROPE_SEGMENT = preload("res://components/rope/rope_segment.tscn")
+const ROPE_END = preload("res://components/rope/rope_end.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	addRopes(4) # Replace with function body.
-	
+
 func addRopes(numRopes):
 	for i in range(numRopes):
 		var ropeContainer = Node2D.new()
 		add_child(ropeContainer)
 		addRopeSegments(ropeContainer)
 		ropeContainer.rotate(i * 0.1)
-		
+
 func addRopeSegments(parent):
-	var head = $RopeHolder
-	var tail =  $RopeHolder
-	var pos = Vector2($RopeHolder.position.x, $RopeHolder.position.y)
-	print(ropeLength)
+	var holder = $RopeHolder
+	var previousSegment = holder
+	var pos = Vector2(holder.position.x, holder.position.y)
+
 	for i in range(ropeLength):
-		print(pos)
 		var joint = PinJoint2D.new()
-		joint.position = pos if i == 0 else Vector2(pos.x + ropeSegmentWidth/2, pos.y)
+		joint.position = Vector2(pos.x + ropeSegmentWidth * 0.5, pos.y)
 		parent.add_child(joint)
-		joint.node_b = tail.get_path()
-		var newRopeSegment = ROPE_SEGMENT.instantiate()
-		if i > 0:
-			newRopeSegment.collision_layer = 1
-			newRopeSegment.collision_mask = 2
+
+		var newRopeSegment
+		if i == ropeLength - 1:
+			newRopeSegment = ROPE_END.instantiate()
 		else:
-			newRopeSegment.collision_layer = 1
-			newRopeSegment.collision_mask = 2
-		pos = Vector2(pos.x + ropeSegmentWidth/2, pos.y) if i == 0 else Vector2(pos.x + ropeSegmentWidth, pos.y)
-		newRopeSegment.position = Vector2(pos.x, pos.y)
-		
+			newRopeSegment = ROPE_SEGMENT.instantiate()
+
+		newRopeSegment.collision_layer = 1
+		newRopeSegment.collision_mask = 2
+
+		pos = Vector2(pos.x + ropeSegmentWidth, pos.y)
+		newRopeSegment.position = pos
 		parent.add_child(newRopeSegment)
-		joint.node_a = newRopeSegment.get_path()
-		print(joint.node_a)
-		tail = newRopeSegment
-		
+
+		joint.node_a = previousSegment.get_path()
+		joint.node_b = newRopeSegment.get_path()
+
+		var distance_factor = float(i) / float(ropeLength - 1)
+		joint.softness = jointStiffness * (0.5 + distance_factor * 1.5)
+		joint.bias = 0.9 - distance_factor * 0.3
+
+		previousSegment = newRopeSegment
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
