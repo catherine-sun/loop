7a04d993324846bae67fd0ca8c37beaf500495a4
Penguinfami <christine.spyphone.zhao@gmail.com>
2025-08-02 00:49:44 -0400

    please work

--- scripts/rope/rope_end.gd
+++ scripts/rope/rope_end.gd
@@ -5,18 +5,34 @@ var of = Vector2.ZERO
 var viewport_size
 var size
 
+var totalDistance = 0
+var distOfLastNewSegment = 0
+
+signal new_segment_please
+
+var id = -1
 func _ready() -> void:
+	of = position
 	size = $CollisionShape2D.shape.size
 	viewport_size = get_viewport_rect().size
 
 func _process(_delta: float) -> void:
 	if dragging:
 		var pos = get_global_mouse_position() - of
+		var prev = Vector2(position.x, position.y)
 		position = Vector2(
 			clamp(pos.x, size.x/2, viewport_size.x - size.x/2),
 			clamp(pos.y, size.y/2, viewport_size.y - size.y/2)
 		)
+		distOfLastNewSegment += sqrt(pow(position.x - prev.x, 2) + pow(position.y - prev.y, 2) )
+		if (distOfLastNewSegment > 120):
+			distOfLastNewSegment = 0
+			print("Emitting signal")
+			emit_signal("new_segment_please", id)
 
+func setId(i):
+	id = i
+	
 func _on_button_button_down() -> void:
 	dragging = true
 	of = get_global_mouse_position() - global_position
--- scripts/rope/rope_generator.gd
+++ scripts/rope/rope_generator.gd
@@ -4,10 +4,14 @@ extends Node2D
 @export var ropeSegmentWidth: int
 
 const ROPE_SEGMENT = preload("res://components/rope/rope_segment.tscn")
+const ROPE_END_SEGMENT = preload("res://components/rope/rope_end.tscn")
+
+var ropeHolders = []
+var ropeContainers = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	addRopes(4) # Replace with function body.
+	addRopes(1) # Replace with function body.
 	
 func addRopes(numRopes):
 	for i in range(numRopes):
@@ -15,34 +19,66 @@ func addRopes(numRopes):
 		add_child(ropeContainer)
 		addRopeSegments(ropeContainer)
 		ropeContainer.rotate(i * 0.1)
+		ropeContainers.append(ropeContainer)
+		
+func addNewSegment(flag):
+	var parent = ropeContainers[flag]
+	print("Called add new segment " + str(flag))
+	var ropeHolderJoint = ropeHolders[flag]
+	var joint = PinJoint2D.new()
+	joint.position = Vector2($RopeHolder.position.x,$RopeHolder.position.y)
+	var joint2 = PinJoint2D.new()
+	joint2.position = Vector2(ropeHolderJoint.position.x + ropeSegmentWidth, ropeHolderJoint.position.y)
+	parent.add_child(joint)
+	joint.node_b = ropeHolderJoint.node_a
+	var newRopeSegment = ROPE_SEGMENT.instantiate()
+	parent.add_child(newRopeSegment)
+	newRopeSegment.collision_layer = 1
+	newRopeSegment.collision_mask = 2
+	newRopeSegment.position = Vector2($RopeHolder.position.x + ropeSegmentWidth/2, $RopeHolder.position.y)
+	ropeContainers[flag].add_child(newRopeSegment)
+	joint.node_a = newRopeSegment.get_path()
+	
+	joint2.node_b = newRopeSegment.get_path()
+	var newRopeSegment2 = ROPE_SEGMENT.instantiate()
+	newRopeSegment2.collision_layer = 1
+	newRopeSegment2.collision_mask = 2
+	newRopeSegment2.position = Vector2($RopeHolder.position.x + ropeSegmentWidth/2, $RopeHolder.position.y)
+	parent.add_child(newRopeSegment2)
+	parent.add_child(joint2)
+
+	joint2.node_a = newRopeSegment2.get_path()
+	print(joint.node_a)
+	ropeHolderJoint.node_a = newRopeSegment2.get_path()
 		
 func addRopeSegments(parent):
 	var head = $RopeHolder
 	var tail =  $RopeHolder
 	var pos = Vector2($RopeHolder.position.x, $RopeHolder.position.y)
-	print(ropeLength)
 	for i in range(ropeLength):
 		print(pos)
 		var joint = PinJoint2D.new()
+		if (i == 0):
+			ropeHolders.append(joint)
 		joint.position = pos if i == 0 else Vector2(pos.x + ropeSegmentWidth/2, pos.y)
 		parent.add_child(joint)
 		joint.node_b = tail.get_path()
-		var newRopeSegment = ROPE_SEGMENT.instantiate()
-		if i > 0:
-			newRopeSegment.collision_layer = 1
-			newRopeSegment.collision_mask = 2
-		else:
-			newRopeSegment.collision_layer = 1
-			newRopeSegment.collision_mask = 2
+		var newRopeSegment = ROPE_SEGMENT.instantiate() if i < ropeLength - 1 else ROPE_END_SEGMENT.instantiate()
+		newRopeSegment.collision_layer = 1
+		newRopeSegment.collision_mask = 2
 		pos = Vector2(pos.x + ropeSegmentWidth/2, pos.y) if i == 0 else Vector2(pos.x + ropeSegmentWidth, pos.y)
 		newRopeSegment.position = Vector2(pos.x, pos.y)
-		
 		parent.add_child(newRopeSegment)
 		joint.node_a = newRopeSegment.get_path()
 		print(joint.node_a)
 		tail = newRopeSegment
 		
+		if i == ropeLength - 1:
+			newRopeSegment.setId(ropeHolders.size()-1)
+			newRopeSegment.connect("new_segment_please", addNewSegment)
 
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
