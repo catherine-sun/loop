e8893bb076cd4f97e0cd76fb676117452c9cbc1f
Penguinfami <christine.spyphone.zhao@gmail.com>
2025-08-02 16:57:20 -0400

    feat: knot collisions

--- scripts/rope/rope_generator.gd
+++ scripts/rope/rope_generator.gd
@@ -4,9 +4,14 @@ extends Node2D
 @export var ropeLength: int
 @export var ropeSegmentWidth: int
 @export var jointStiffness: float
+@export var ropeId: String
 
 const ROPE_SEGMENT = preload("res://components/rope/rope_segment.tscn")
 const ROPE_END = preload("res://components/rope/rope_end.tscn")
+
+var ropes = []
+
+var overlappingBodies = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,10 +23,21 @@ func addRopes(numRopes):
 	for i in range(numRopes):
 		var ropeContainer = Node2D.new()
 		add_child(ropeContainer)
-		addRopeSegments(ropeContainer)
+		var newRopeObj = {
+			"parent": ropeContainer,
+			"joints": [],
+			"segments": [],
+			"crosses": [],
+			"end": null,
+			"currentCross": null,
+			"ropeId": ropeId
+		}
+		ropes.append(newRopeObj)
+		addRopeSegments(newRopeObj)
 		ropeContainer.rotate(i * 0.1)
 
-func addRopeSegments(parent):
+func addRopeSegments(rope):
+	var parent = rope["parent"]
 	var holder = $RopeHolder
 	var previousSegment = holder
 	var pos = Vector2(holder.position.x, holder.position.y)
@@ -34,8 +50,16 @@ func addRopeSegments(parent):
 		var newRopeSegment
 		if i == ropeLength - 1:
 			newRopeSegment = ROPE_END.instantiate()
+			rope["end"] = newRopeSegment
 		else:
 			newRopeSegment = ROPE_SEGMENT.instantiate()
+			newRopeSegment.name = "RopeSegment" + str(i)
+			rope["segments"].append(newRopeSegment)
+			newRopeSegment.setRopeIds({
+				"ropeId": ropeId,
+				"segmentId": i
+			})
+
 
 		newRopeSegment.collision_layer = 1
 		newRopeSegment.collision_mask = 2
@@ -52,7 +76,30 @@ func addRopeSegments(parent):
 		joint.bias = 0.9 - distance_factor * 0.3
 
 		previousSegment = newRopeSegment
+		
+		rope["joints"].append(joint)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	#print(getOverlappingRopes())
 	pass
+	
+func getOverlappingRopes():
+	if len(ropes) > 0:
+		var overlappingRopes = []
+		var rope = ropes[0]
+		var prevSegment = null
+		var nextSegment = null
+		var segments = rope["segments"]
+		var numSegments = segments.size()
+		
+		for i in range(numSegments):
+			nextSegment = segments[i+1] if i < numSegments - 1 else null
+			prevSegment = segments[i-1] if i > 0 else null 
+			var segment = segments[i]
+			var segmentOverlappingNodes = segment.get_node("Area2D").get_overlapping_bodies().map(func(n): return n.getId()["ropeId"] if "RopeSegment" in n.name and (n.getId()["ropeId"] != segment.getId()["ropeId"] or abs(n.getId()["segmentId"] - segment.getId()["segmentId"]) > 2) else null)
+			segmentOverlappingNodes = segmentOverlappingNodes.filter(func(x): return x != null and x != "")
+			overlappingRopes = overlappingRopes + segmentOverlappingNodes.filter(func(x): return x != null and x != "")
+		return overlappingRopes
+	return []
+		
