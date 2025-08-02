3f439282eae777d36c61f702bd9ee76e0d4d8593
Penguinfami <christine.spyphone.zhao@gmail.com>
2025-08-01 21:05:14 -0400

    feat: fixed collision

--- scripts/rope/rope_generator.gd
+++ scripts/rope/rope_generator.gd
@@ -11,9 +11,12 @@ func _ready() -> void:
 	
 func addRopes(numRopes):
 	for i in range(numRopes):
-		addRopeSegments()
+		var ropeContainer = Node2D.new()
+		add_child(ropeContainer)
+		addRopeSegments(ropeContainer)
+		ropeContainer.rotate(i * 0.1)
 		
-func addRopeSegments():
+func addRopeSegments(parent):
 	var head = $RopeHolder
 	var tail =  $RopeHolder
 	var pos = Vector2($RopeHolder.position.x, $RopeHolder.position.y)
@@ -22,13 +25,19 @@ func addRopeSegments():
 		print(pos)
 		var joint = PinJoint2D.new()
 		joint.position = pos if i == 0 else Vector2(pos.x + ropeSegmentWidth/2, pos.y)
-		add_child(joint)
+		parent.add_child(joint)
 		joint.node_b = tail.get_path()
 		var newRopeSegment = ROPE_SEGMENT.instantiate()
+		if i > 0:
+			newRopeSegment.collision_layer = 1
+			newRopeSegment.collision_mask = 2
+		else:
+			newRopeSegment.collision_layer = 1
+			newRopeSegment.collision_mask = 2
 		pos = Vector2(pos.x + ropeSegmentWidth/2, pos.y) if i == 0 else Vector2(pos.x + ropeSegmentWidth, pos.y)
 		newRopeSegment.position = Vector2(pos.x, pos.y)
 		
-		add_child(newRopeSegment)
+		parent.add_child(newRopeSegment)
 		joint.node_a = newRopeSegment.get_path()
 		print(joint.node_a)
 		tail = newRopeSegment
