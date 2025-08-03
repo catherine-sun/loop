11ca76dff448411b03b21f276f1991fca84d2e78
Penguinfami <christine.spyphone.zhao@gmail.com>
2025-08-02 23:45:51 -0400

    in-prog

--- scripts/main_menu/main_menu.gd
+++ scripts/main_menu/main_menu.gd
@@ -1,6 +1,7 @@
 extends PanelContainer
 
 const SETTINGS = preload("res://scenes/settings_menu.tscn")
+const PLAY = preload("res://scenes/level_select.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,9 +11,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+func _on_play_button_pressed():
+	get_tree().change_scene_to_packed(PLAY)
 
 func _on_endless_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_packed(SETTINGS)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()  # Replace with function body.
--- scripts/main_menu/main_menu_margin.gd
+++ scripts/main_menu/main_menu_margin.gd
@@ -1,8 +1,8 @@
 extends MarginContainer
 
 func _ready():
-	var margin_value = 100
-	add_theme_constant_override("margin_top", margin_value)
+	var margin_value = 50
+	add_theme_constant_override("margin_top", 390)
 	add_theme_constant_override("margin_left", margin_value)
 	add_theme_constant_override("margin_bottom", margin_value)
 	add_theme_constant_override("margin_right", margin_value)
