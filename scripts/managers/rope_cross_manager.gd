5ca7226c61674faa153572b32b6b619b1e6c598b
Christine Zhao <66979909+Penguinfami@users.noreply.github.com>
2025-08-03 11:25:36 -0400

    HOTFIX: Update rope_cross_manager.gd

--- scripts/managers/rope_cross_manager.gd
+++ scripts/managers/rope_cross_manager.gd
@@ -95,8 +95,10 @@ func init(ropeIds):
 func pause():
 	isActive = false
 	
-func isInACross(ropeId):
-	return crosses[ropeId["ropeId"]].any(func(x): return x["segment"]["segmentId"] == ropeId["segmentId"])
+func isInACross(rope_id, segment):
+	if crosses.has(rope_id):
+		return crosses[rope_id].any(func(x): return x["segment"]["segmentId"] == segment.segment_id)
+	return false
 	
 	
 
